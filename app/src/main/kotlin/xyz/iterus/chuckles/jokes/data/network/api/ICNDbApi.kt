bed8e2be99e55ccd049efa562ed22c266638b49e
iterus <iterus@protonmail.com>
2021-05-11 01:25:10 +0300

    Fix incorrect quotation marks escaping

--- app/src/main/kotlin/xyz/iterus/chuckles/jokes/data/network/api/ICNDbApi.kt
+++ app/src/main/kotlin/xyz/iterus/chuckles/jokes/data/network/api/ICNDbApi.kt
@@ -2,19 +2,26 @@ package xyz.iterus.chuckles.jokes.data.network.api
 
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 import xyz.iterus.chuckles.jokes.data.network.response.ICNDbResponse
 import xyz.iterus.chuckles.jokes.domain.model.Joke
 
 interface ICNDbApi {
 
     @GET("jokes/random")
-    suspend fun getRandomJoke(): ICNDbResponse<Joke>
+    suspend fun getRandomJoke(@Query("escape") escape: String? = "javascript"): ICNDbResponse<Joke>
 
     @GET("jokes/random/{number}")
-    suspend fun getRandomJokes(@Path("number") numberOfJokes: Int): ICNDbResponse<List<Joke>>
+    suspend fun getRandomJokes(
+        @Path("number") numberOfJokes: Int,
+        @Query("escape") escape: String? = "javascript"
+    ): ICNDbResponse<List<Joke>>
 
     @GET("jokes/{id}")
-    suspend fun getJokeById(@Path("id") id: Int): ICNDbResponse<Joke>
+    suspend fun getJokeById(
+        @Path("id") id: Int,
+        @Query("escape") escape: String? = "javascript"
+    ): ICNDbResponse<Joke>
 
     @GET("jokes/count")
     suspend fun getNumberOfJokes(): ICNDbResponse<Int>
