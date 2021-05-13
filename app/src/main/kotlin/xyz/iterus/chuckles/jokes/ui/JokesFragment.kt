07d3542d11b01f99002c4ea0364ee9905079da06
iterus <iterus@protonmail.com>
2021-05-13 16:42:42 +0300

    Fix ViewBinding and Adapter memory leaks

--- app/src/main/kotlin/xyz/iterus/chuckles/jokes/ui/JokesFragment.kt
+++ app/src/main/kotlin/xyz/iterus/chuckles/jokes/ui/JokesFragment.kt
@@ -6,60 +6,69 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
-import org.koin.android.ext.android.inject
+import org.koin.android.ext.android.get
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import xyz.iterus.chuckles.R
 import xyz.iterus.chuckles.databinding.FragmentJokesBinding
 
 class JokesFragment : Fragment(R.layout.fragment_jokes) {
 
-    private lateinit var binding: FragmentJokesBinding
     private val model: JokesViewModel by viewModel()
-    private val jokesAdapter: JokesAdapter by inject()
+    private var jokesAdapter: JokesAdapter? = get()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentJokesBinding.inflate(inflater, container, false)
+        val binding = FragmentJokesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentJokesBinding.bind(view)
 
         binding.jokes.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = jokesAdapter
         }
 
-        subscribeObservers()
+        setupListeners(binding)
+        setupViewModelObservers(binding)
     }
 
-    private fun subscribeObservers() {
-        with (model) {
-            jokes.observe(viewLifecycleOwner, {
-                jokesAdapter.submitList(it)
-            })
+    override fun onDestroyView() {
+        super.onDestroyView()
+        jokesAdapter = null
+    }
 
-            loading.observe(viewLifecycleOwner, {
-                showProgressBar(it)
-            })
 
-            jokesError.observe(viewLifecycleOwner, {
-                showInputError(it)
-            })
-        }
-
+    private fun setupListeners(binding: FragmentJokesBinding) {
         binding.reloadButton.setOnClickListener {
             val count = binding.jokesCount.text.toString()
             model.reloadJokes(count)
         }
     }
 
-    private fun showProgressBar(isDisplayed: Boolean) {
+    private fun setupViewModelObservers(binding: FragmentJokesBinding) {
+        with (model) {
+            jokes.observe(viewLifecycleOwner, {
+                jokesAdapter?.submitList(it)
+            })
+
+            loading.observe(viewLifecycleOwner, {
+                showProgressBar(it, binding)
+            })
+
+            jokesError.observe(viewLifecycleOwner, {
+                showInputError(it, binding)
+            })
+        }
+    }
+
+    private fun showProgressBar(isDisplayed: Boolean, binding: FragmentJokesBinding) {
         val visibility = if (isDisplayed) View.VISIBLE else View.GONE
         binding.loading.visibility = visibility
     }
 
-    private fun showInputError(error: JokesViewModel.JokesError) {
+    private fun showInputError(error: JokesViewModel.JokesError, binding: FragmentJokesBinding) {
         binding.jokesCount.error = when (error) {
             is JokesViewModel.JokesError.None -> null
             is JokesViewModel.JokesError.EmptyJokesCount -> getString(R.string.error_input_empty)
